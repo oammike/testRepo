b0b6ab483a904d673d0455c9688ca68ea4191395
oammike <mpamero@openaccessbpo.com>
2018-10-19 14:27:58 +0800

    test branch1

--- resources/views/reports/formBuilder-index.blade.php
+++ resources/views/reports/formBuilder-index.blade.php
@@ -22,43 +22,7 @@
 
 
           
-               
-
-     
-          <div class="row">
-            <div class="col-lg-1"></div>
-            <div class="col-lg-10">
-
-              <div id="stage1" class="build-wrap"></div>
-              <form class="render-wrap"></form>
-              <button id="edit-form">Edit Form</button>
-              <div class="action-buttons">
-                <h2>Actions</h2>
-                <button id="showData" type="button">Show Data</button>
-                <button id="clearFields" type="button">Clear All Fields</button>
-                <button id="getData" type="button">Get Data</button>
-                <button id="getXML" type="button">Get XML Data</button>
-                <button id="getJSON" type="button">Get JSON Data</button>
-                <button id="getJS" type="button">Get JS Data</button>
-                <button id="setData" type="button">Set Data</button>
-                <button id="addField" type="button">Add Field</button>
-                <button id="removeField" type="button">Remove Field</button>
-                <button id="testSubmit" type="submit">Test Submit</button>
-                <button id="resetDemo" type="button">Reset Demo</button>
-                
-              </div>
-
-
-            </div>
-            <div class="col-lg-1"></div>
-
-            
-
-            
-
-          </div><!-- end row -->
-
-
+         
 
 
 
@@ -76,15 +40,6 @@
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-
-<script src="{{URL::asset('public/js/formBuilder/vendor.js')}}"></script>
-<script src="{{URL::asset('public/js/formBuilder/form-builder.min.js')}}"></script>
-<script src="{{URL::asset('public/js/formBuilder/form-render.min.js')}}"></script>
-<script src="{{URL::asset('public/js/formBuilder/demo.js')}}"></script>
-
-
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.1/jquery.rateyo.min.js"></script>
- -->
 
 
 <!-- Page script -->
